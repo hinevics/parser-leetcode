28b9d12f4f84f06192b65b88aceeb156a4a45d38
Sasha Khinevich <a.khinevich@cft.ru>
2023-06-03 00:55:40 +0300

    add func for get path file

--- src/get_algs.py
+++ src/get_algs.py
@@ -4,10 +4,12 @@ import pathlib
 def get_file_path(directory_path: pathlib.PosixPath) -> list[pathlib.PosixPath]:
     # returns a list of all files
     file_list = list(directory_path.glob('*.json'))
-    
-    for file_path in file_list
+    for file_path in file_list:
+        yield file_path
 
 
 def get_alg_url(directory_path: pathlib.PosixPath) -> dict[str, str]:
     # returns a link to the algorithm and the name of the algorithm
     file_path = get_file_path(directory_path)
+    for fp in file_path:
+        print(fp)
