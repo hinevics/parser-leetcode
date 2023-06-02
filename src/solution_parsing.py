daa7099ad2af1bd8f7b9420bdc020940bd4add95
Sasha Khinevich <a.khinevich@cft.ru>
2023-06-03 01:04:05 +0300

    the beginning of the package implementation

---
+++ src/solution_parsing.py
@@ -0,0 +1,12 @@
+# Module for collecting solutions of the algorithm
+
+from get_algs import get_alg_url
+from myconfig import DATA_PAGE_ALGS_PATH
+
+
+def mian():
+    pass
+
+
+if __name__ == "__main__":
+    pass
