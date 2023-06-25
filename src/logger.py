8b7cfe5550e4cd81e7c1f71e2d932885365a61fd
Sasha Khinevich <hinevics@gmail.com>
2023-06-25 13:57:31 +0300

    ✨feat: fix formatter for logs

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,9 @@ from myconfig import LOG_PATH, LOG_ERROR_PATH
 # format logs
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 color_format = '%(log_color)s' + log_format
-formatter = colorlog.ColoredFormatter(color_format)
+formatter_console = colorlog.ColoredFormatter(color_format)
+formatter_file = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 # all logs
@@ -16,8 +18,8 @@ file_handler = logging.FileHandler(LOG_PATH)
 file_handler.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter_file)
+console_handler.setFormatter(formatter_console)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
@@ -27,8 +29,8 @@ error_logger.setLevel(logging.ERROR)
 file_handler_error = logging.FileHandler(LOG_ERROR_PATH)
 console_handler_error = logging.StreamHandler()
 console_handler_error.setLevel(logging.ERROR)
-file_handler_error.setFormatter(formatter)
-console_handler_error.setFormatter(formatter)
+file_handler_error.setFormatter(formatter_file)
+console_handler_error.setFormatter(formatter_console)
 error_logger.addHandler(file_handler_error)
 error_logger.addHandler(console_handler_error)
 
@@ -39,3 +41,4 @@ if __name__ == "__main__":
     logger.warning('This is a warning message')
     logger.error('This is an error message')
     logger.critical('This is a critical message')
+    error_logger.error('This is an error message for error_logger')
