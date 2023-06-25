a81aa6d1b3b4109c178527a6fb6f59d03019b9a4
Sasha Khinevich <hinevics@gmail.com>
2023-06-25 13:38:46 +0300

    ✨feat: add new consol logs

--- src/logger.py
+++ src/logger.py
@@ -1,21 +1,36 @@
 import logging
 import colorlog
 
-from myconfig import LOG_PATH
+from myconfig import LOG_PATH, LOG_ERROR_PATH
 
+# format logs
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+color_format = '%(log_color)s' + log_format
+formatter = colorlog.ColoredFormatter(color_format)
+
+
+# all logs
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 file_handler = logging.FileHandler(LOG_PATH)
 file_handler.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
-log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-color_format = '%(log_color)s' + log_format
-formatter = colorlog.ColoredFormatter(color_format)
 file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
+
+# error logs
+error_logger = logging.getLogger('error_logger')
+error_logger.setLevel(logging.ERROR)
+file_handler_error = logging.FileHandler(LOG_ERROR_PATH)
+console_handler_error = logging.StreamHandler()
+console_handler_error.setLevel(logging.ERROR)
+file_handler_error.setFormatter(formatter)
+console_handler_error.setFormatter(formatter)
+error_logger.addHandler(file_handler_error)
+error_logger.addHandler(console_handler_error)
 
 
 if __name__ == "__main__":
