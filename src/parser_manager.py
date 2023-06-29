df79b943b61a0b96190c9cb50da29ac41f84e323
Sasha Khinevich <hinevics@gmail.com>
2023-06-29 16:52:07 +0300

    clean project

--- src/parser_manager.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from bs4.element import Tag
-
-
-def parser_divs(content, class_: str) -> list[Tag]:
-    soup = BeautifulSoup(content, 'html.parser')
-    divs = soup.find_all(class_=class_)
-    return divs
